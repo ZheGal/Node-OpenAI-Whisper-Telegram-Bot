3acc55d48fe1e0d586f8de993f24a07547d8deb6
Evgeniy Galkovskiy <zhegal@gmail.com>
2023-03-15 00:28:32 +0200

    get file link on upload to telegram

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
-const app = () => {
-    console.log('I am started!');
-};
+import { Telegraf } from 'telegraf';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-app();
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+bot.on('audio', async (ctx) => {
+  const audio = ctx.update.message.audio;
+  const file = await bot.telegram.getFileLink(audio.file_id);
+  console.log(file.href);
+});
+
+bot.launch();
